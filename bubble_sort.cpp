f954b30bdb7ec89f2aee57f70820f508231bd689
mariano <mar05mora@estudiantec.cr>
2025-10-31 03:18:06 -0600

    Fix del bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
-void bubbleSort(int array[], int n){
-    for(int i = 0; i < n-1; i++){
-        for(int j = 0; j < n-i-1; j++){
-            if(array[j] > array[j+1]){
-                // Swap array[j] and array[j+1]
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambiar
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int main(){
-    int array[] = {3,9,8,7,1};
-    int n = size(array);
+int main() {
+    int array[] = {3, 9, 8, 7, 4, 5,7,3,2,1};
+    int n = sizeof(array) / sizeof(array[0]);
+
     bubbleSort(array, n);
+
     cout << "Sorted array: ";
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << array[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
+

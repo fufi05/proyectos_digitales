0406ebb7a80a302a3094b5ae5e5b2384feb9b01b
fufi05 <segurag1105@gmail.com>
2025-12-06 20:43:09 -0600

    Memoria caché en proceso. Faltan comentarios en el código

--- pr3/design/module_mem_principal.sv
+++ pr3/design/module_mem_principal.sv
@@ -1,8 +1,12 @@
+// archivo: module_mem_principal.sv
+// Módulo de memoria principal modelada por bytes. Tamaño: MEM_SIZE_BYTES (2^16) 
+
 module module_mem_principal #(
+    // Parámetros de tamaño
     parameter int ADDR_WIDTH = 16, // tamaño de la dirección de memoria
     parameter int SIZE_BLOCK = 256, // Tamaño de los bloques de memoria en bits
     parameter int WORD_SIZE = 32, // Tamaño de la palabra en bits 
-    parameter int MEM_SIZE_BYTES = (1 << ADDR_WIDTH) // Tamaño total de la memoria en bytes 
+    parameter int MEM_SIZE_BYTES = (1 << ADDR_WIDTH) // Tamaño total de la memoria en bytes (2^16)
 )(
 
     input logic clk,
@@ -22,7 +26,7 @@ module module_mem_principal #(
     output logic                     wr_bytes_ack  // Bandera de reconocimiento de escritura de bloque 
 );
 
-localparam int MEM_BYTES = MEM_SIZE_BYTES;
+    localparam int MEM_BYTES = MEM_SIZE_BYTES;
     // Definición de la memoria principal en bytes
     logic [7:0] mem [0:MEM_BYTES-1];
 
@@ -50,7 +54,8 @@ localparam int MEM_BYTES = MEM_SIZE_BYTES;
                 for (b = 0; b < 32; b++) begin
                     rd_block_data[8*b +: 8] <= mem[rd_block_base_addr + b];
                 end
-            end else begin
+            end 
+            else begin
                 rd_block_ack <= 1'b0;
             end
 
@@ -67,5 +72,4 @@ localparam int MEM_BYTES = MEM_SIZE_BYTES;
             end
         end
     end
-
 endmodule 
